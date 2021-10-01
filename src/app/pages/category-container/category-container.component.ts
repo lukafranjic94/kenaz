a11742996d4202a412e97dd96f0d59589b80b732
Luka Franjić <lukafranjic94@gmail.com>
2021-10-01 12:27:46 +0200

    Display author name on category page articles

--- src/app/pages/category-container/category-container.component.ts
+++ src/app/pages/category-container/category-container.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Article } from 'src/app/services/article/article.model';
 import { ArticleService } from 'src/app/services/article/article.service';
+import { Author } from 'src/app/services/author/author.model';
+import { AuthorService } from 'src/app/services/author/author.service';
+
+interface ITemplateData {
+  article: Article;
+  author: Author;
+}
 
 @Component({
   selector: 'app-category-container',
@@ -9,19 +16,35 @@ import { ArticleService } from 'src/app/services/article/article.service';
   styleUrls: ['./category-container.component.scss'],
 })
 export class CategoryContainerComponent implements OnInit {
+  public templateData: Array<ITemplateData>;
   public articles: Array<Article>;
   constructor(
     private route: ActivatedRoute,
-    private articleService: ArticleService
+    private articleService: ArticleService,
+    private authorService: AuthorService
   ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params: ParamMap) => {
       const categoryName: string | null = params.get('name');
-      if (categoryName) {
-        this.articles =
-          this.articleService.getArticlesForCategoryName(categoryName);
+      if (!categoryName) {
+        throw new Error('category not found');
       }
+      this.templateData = this.articleService
+        .getArticlesForCategoryName(categoryName)
+        .map((article: Article) => {
+          let author = this.authorService.getAuthor(article.authorId);
+          if (!author) {
+            throw new Error('no author');
+          }
+          return {
+            article,
+            author,
+          };
+        });
+      this.articles = this.templateData.map(
+        (templateData: ITemplateData) => templateData.article
+      );
     });
   }
 }
