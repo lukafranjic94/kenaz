d1a6cb5909319b60b26a7bcc13d05da9deb70b10
Luka Franjić <lukafranjic94@gmail.com>
2021-10-03 18:38:19 +0200

    Adjust category-articles styles

--- src/app/pages/home-container/components/category-articles/category-articles.component.ts
+++ src/app/pages/home-container/components/category-articles/category-articles.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from 'src/app/services/article/article.model';
+import { Category } from 'src/app/services/category/category.model';
+import { CategoryService } from 'src/app/services/category/category.service';
 
 @Component({
   selector: 'app-category-articles',
@@ -12,14 +14,21 @@ export class CategoryArticlesComponent implements OnInit {
   @Input() title: string;
   @Input() type: string = 'vertical';
   @Input() header: string = 'link';
-  public dateOptions: Intl.DateTimeFormatOptions;
-  constructor() {
-    this.dateOptions = {
-      month: 'long',
-      day: 'numeric',
-      year: 'numeric',
-    };
-  }
+  public categoryUrl: Array<string>;
+  public dateOptions: Intl.DateTimeFormatOptions = {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  };
+  constructor(private categoryService: CategoryService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const category: Category | undefined = this.categoryService.getCategory(
+      this.articles[0].categoryId
+    );
+    if (!category) {
+      throw new Error('Bad category');
+    }
+    this.categoryUrl = ['categories', category.name.toLowerCase()];
+  }
 }
